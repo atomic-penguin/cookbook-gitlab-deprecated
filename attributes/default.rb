d877c25baa12b5b4bead4bb16a1f677e42185509
dscheu <daniel@dash.sh>
2012-12-17 16:08:14 +0100

    Updated gitlab and gitolite url

--- attributes/default.rb
+++ attributes/default.rb
@@ -25,7 +25,7 @@ default['gitlab']['app_home'] = "#{node['gitlab']['home']}/gitlab"
 
 # Set github URL for gitlab
 default['gitlab']['gitlab_url'] = "https://github.com/gitlabhq/gitlabhq.git"
-default['gitlab']['gitlab_branch'] = "v3.1.0"
+default['gitlab']['gitlab_branch'] = "stable"
 
 # Required packages for Gitlab
 case node['platform']
