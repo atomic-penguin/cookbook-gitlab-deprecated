2d7c7a07489640f8cf40b9378810ab9625d185a8
Jeff Morgan and Mahmoud Abounassif <pair+jmorgan+mabounassif@projectchelsea.com>
2013-03-03 17:39:22 -0800

    installing ruby shadow

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,6 +19,11 @@
 #
 
 # Add the gitlab user
+gem_package "ruby-shadow" do
+  gem_binary  "/opt/opscode/embedded/bin/gem"
+  action :install
+end
+
 user node['gitlab']['user'] do
   comment "Gitlab User"
   home node['gitlab']['home']
