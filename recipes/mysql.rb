528ac6fe1393d438939f4a50517ee51afcf95d29
Takayuki Hirai <hirai@logarhythm.jp>
2013-12-18 16:16:20 +0900

    fix invalid name

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -50,7 +50,7 @@ end
 mysql_database_user database_user do
   connection      database_connection
   password        database_password
-  userhost        database_userhost
+  host            database_userhost
   database_name   database
   action          :create
 end
