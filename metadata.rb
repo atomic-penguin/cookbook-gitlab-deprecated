421041824cb12c22d5c6f762cc0b4182eb6a3a68
Eric G. Wolfe <eric.wolfe@gmail.com>
2014-03-12 10:26:07 -0400

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ license          'Apache 2.0'
 description      'Installs/Configures gitlab'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 name             'gitlab'
-version          '6.4.5'
+version          '6.4.6'
 
 %w[build-essential zlib readline ncurses git openssh redisio xml
    ruby_build certificate database logrotate mysql nginx
